7a53087b1ec0df8a0c7a92ca8c7cf2765d32a6b7
Nicolas Iskandar <iskandar.nicolas1@gmail.com>
2024-06-12 18:24:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ function generatePassword() {
   return password;
 }
 
-generateBtnEl.addEventListener("click", function () {
+generateBtnEl.addEventListener("clck", function () {
   firstGeneratedPasswordValue = generatePassword();
   secondGeneratedPasswordValue = generatePassword();
   firstGeneratedPassword.textContent = firstGeneratedPasswordValue;
